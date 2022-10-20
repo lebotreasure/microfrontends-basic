130debe2a5c86d0bb99adab456803a8323c3103d
lebohangkhaeane <khaeane@thedigitalacademy.co.za>
2022-10-20 22:58:40 +0200

    Microfrontends basic

---
+++ packages/helloReact/config/webpack.common.js
@@ -0,0 +1,29 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+                exclude: /node_modules/,
+                use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    }
+}
